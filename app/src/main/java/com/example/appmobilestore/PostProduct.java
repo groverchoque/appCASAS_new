1f75bfc0ddd7ed37833de41640feef82ef272329
groverchoque <grover_chato@hotmail.com>
2019-12-18 19:49:01 -0400

    fotos multiples

--- app/src/main/java/com/example/appmobilestore/PostProduct.java
+++ app/src/main/java/com/example/appmobilestore/PostProduct.java
@@ -1,12 +1,19 @@
 package com.example.appmobilestore;
 
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.FileProvider;
 
+import android.Manifest;
+import android.content.ClipData;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
@@ -15,9 +22,11 @@ import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -36,6 +45,7 @@ import org.json.JSONObject;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.List;
 
 import cz.msebera.android.httpclient.Header;
 
@@ -46,22 +56,120 @@ public class PostProduct extends AppCompatActivity implements View.OnClickListen
     ImageView imageView;
     TextView latText, logText;
     Button btnCamera, btnPublicar, mapButton;
-
     int RESULT_MAP = 100;
     Double lat,log;
-
     String estado,categoria;
+    //seleccion multiple
+    static final int CODE_GALERIA=200;
+    static final int CODE_PERMISSION=100;
+
+
+    Button btnTaer;
+    Button btnEnv;
+    List<String> imagesEncodedList;private GridView gvGallery;
+    private Adapterimg galleryAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_product);
         loadComponents();
-
-
         Methods.validarPermisos(this,this);
 
+        // inicio cargar varias imagenes
+        btnEnv=findViewById(R.id.enviar);
+        btnTaer=findViewById(R.id.traer);
+        gvGallery = (GridView)findViewById(R.id.gv);
+        btnTaer.setVisibility(View.INVISIBLE);
+        if(reviewPermissions()){
+            btnTaer.setVisibility(View.VISIBLE);
+        }
+
+        btnTaer.setOnClickListener(this);
+        btnEnv.setOnClickListener(this);
+
+
+
     }
+
+    private boolean reviewPermissions() {
+        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.M){
+            return true;
+        }
+
+        if(this.checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED &&
+                checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED &&
+                checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
+            return true;
+        }
+        requestPermissions(new String [] {Manifest.permission.CAMERA,
+                        Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                CODE_PERMISSION);
+        return false;
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (CODE_PERMISSION == requestCode) {
+            if (permissions.length == 3) {
+                btnTaer.setVisibility(View.VISIBLE);
+            }
+
+        }
+
+    }
+
+
+
+    private String getRealPath(Context context, Uri uri){
+        String path=null;
+        Cursor cursor=context.getContentResolver().query(uri,
+                null,null,null,null);
+        if(cursor!=null){
+            cursor.moveToFirst();
+            int i=cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
+            path=cursor.getString(i);
+            cursor.close();
+        }
+        return path;
+    }
+
+    private void enviarImg() {
+        AsyncHttpClient client=new AsyncHttpClient();
+        RequestParams req=new RequestParams();
+        File[] imgs=new File[imagesEncodedList.size()];
+        if(imagesEncodedList.size()==0){
+            Toast.makeText(getApplicationContext(),"debe de tomar al menos una foto",Toast.LENGTH_LONG).show();
+            return;
+        }else{
+            for(int i=0;i<imagesEncodedList.size();i++){
+                imgs[i]=new File(imagesEncodedList.get(i));
+            }
+            try {
+                req.put("img",imgs);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+        client.post("http://192.168.43.132:8000/user/subir",req,new JsonHttpResponseHandler(){
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                super.onSuccess(statusCode, headers, response);
+                try {
+                    String re=response.getString("message");
+                    Toast.makeText(getApplicationContext(),""+re,Toast.LENGTH_LONG).show();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+
+    //  fin cargar y seleccionar varias fotos
+
 
     private void loadComponents() {
         direcciontxt = findViewById(R.id.direcciontxt1);
@@ -262,7 +370,14 @@ public class PostProduct extends AppCompatActivity implements View.OnClickListen
         } if (v.getId() == R.id.map_button) {
             registerMap();
 
+            //botones seleccion multiple
+        } if(v.getId()==R.id.enviar){
+            enviarImg();
         }
+
+
+
+
     }
      ///camara
     private void registerMap() {
@@ -271,27 +386,15 @@ public class PostProduct extends AppCompatActivity implements View.OnClickListen
     }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
     //DESDE AQUI VA LA PARTE DE LA FOTO
     final int COD_GALERIA=10;
     final int COD_CAMERA=20;
+    final int COD_GALERIA_VARIOS=30;
     String path;
 
     private void cargarImagen() {
 
-        final CharSequence[] opciones={"Tomar Foto","Cargar Imagen","Cancelar"};
+        final CharSequence[] opciones={"Tomar Foto","Cargar Imagen","selecciona varias img","Cancelar"};
         final AlertDialog.Builder alertOpciones=new AlertDialog.Builder(PostProduct.this);
         alertOpciones.setTitle("Seleccione una Opción");
         alertOpciones.setItems(opciones, new DialogInterface.OnClickListener() {
@@ -305,9 +408,25 @@ public class PostProduct extends AppCompatActivity implements View.OnClickListen
                         intent.setType("image/*");
                         startActivityForResult(intent.createChooser(intent,"Seleccione la Aplicación"),COD_GALERIA);
                     }else{
-                        dialogInterface.dismiss();
+                        if (opciones[i].equals("selecciona varias img")){
+                            Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                            intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+                            intent.setType("image/");
+                            //intent.setAction(Intent.ACTION_GET_CONTENT);
+                            startActivityForResult(Intent.createChooser(intent,"Select Picture"), CODE_GALERIA);
+
+
+                        }else {
+                            dialogInterface.dismiss();
+                        }
+
+
                     }
                 }
+
+
+
+
             }
         });
         alertOpciones.show();
@@ -336,43 +455,78 @@ public class PostProduct extends AppCompatActivity implements View.OnClickListen
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode,resultCode,data);
-        if (resultCode==RESULT_OK){
-            switch (requestCode){
-                case COD_GALERIA:
-                    Uri imgPath=data.getData();
-                    imageView.setImageURI(imgPath);
-                    path = Methods.getRealPathFromURI(this,imgPath);
-                    Toast.makeText(PostProduct.this, path, Toast.LENGTH_SHORT).show();
-                    break;
-                case COD_CAMERA:
-                    loadImageCamera();
-                    break;
+               //varias imagenes
+        try {
+            if (resultCode==RESULT_OK){
+                switch (requestCode){
+                    case COD_GALERIA:
+                        Uri imgPath=data.getData();
+                        imageView.setImageURI(imgPath);
+                        path = Methods.getRealPathFromURI(this,imgPath);
+                        Toast.makeText(PostProduct.this, path, Toast.LENGTH_SHORT).show();
+                        break;
+                    case COD_CAMERA:
+                        loadImageCamera();
+                        break;
+                        //
+                }
+            }      if (resultCode == RESULT_OK) {
+                // The user picked a contact.
+                // The Intent's data Uri identifies which contact was selected.
+
+                // Do something with the contact here (bigger example bel;ow)
+                lat = data.getDoubleExtra("lat",0.0);
+                log = data.getDoubleExtra("log",0.0);
+                latText = findViewById(R.id.lat);
+                logText = findViewById(R.id.log);
+                latText.setText(lat.toString());
+                logText.setText(log.toString());
             }
-        }      if (resultCode == RESULT_OK) {
-            // The user picked a contact.
-            // The Intent's data Uri identifies which contact was selected.
+            // When an Image is picked
+            if (requestCode == CODE_GALERIA && resultCode == RESULT_OK) {
+                imagesEncodedList = new ArrayList<String>();
+                if(data.getClipData()!=null){
+                    ClipData mClipData = data.getClipData();
+                    ArrayList<Uri> mArrayUri = new ArrayList<Uri>();
+                    for (int i = 0; i < mClipData.getItemCount(); i++) {
+                        ClipData.Item item = mClipData.getItemAt(i);
+                        Uri uri = item.getUri();
+                        mArrayUri.add(uri);
 
-            // Do something with the contact here (bigger example bel;ow)
-            lat = data.getDoubleExtra("lat",0.0);
-            log = data.getDoubleExtra("log",0.0);
-            latText = findViewById(R.id.lat);
-            logText = findViewById(R.id.log);
+                        imagesEncodedList.add(getRealPath(this,uri));
+                        //Toast.makeText(this,""+getRealPath(this,uri),Toast.LENGTH_LONG).show();
+                        galleryAdapter = new Adapterimg(getApplicationContext(),mArrayUri);
+                        gvGallery.setAdapter(galleryAdapter);
+                        gvGallery.setVerticalSpacing(gvGallery.getHorizontalSpacing());
+                        ViewGroup.MarginLayoutParams mlp = (ViewGroup.MarginLayoutParams) gvGallery
+                                .getLayoutParams();
+                        mlp.setMargins(0, gvGallery.getHorizontalSpacing(), 0, 0);
 
-            latText.setText(lat.toString());
-            logText.setText(log.toString());
+                    }
+                    //Log.v("LOG_TAG", "Selected Images" + mArrayUri.size());
+                } else {
+                    Uri mImageUri=data.getData();
+
+                    imagesEncodedList.add(getRealPath(this,mImageUri));
+
+                    ArrayList<Uri> mArrayUri = new ArrayList<Uri>();
+                    mArrayUri.add(mImageUri);
+                    galleryAdapter = new Adapterimg(getApplicationContext(),mArrayUri);
+                    gvGallery.setAdapter(galleryAdapter);
+                    gvGallery.setVerticalSpacing(gvGallery.getHorizontalSpacing());
+                    ViewGroup.MarginLayoutParams mlp = (ViewGroup.MarginLayoutParams) gvGallery
+                            .getLayoutParams();
+                    mlp.setMargins(0, gvGallery.getHorizontalSpacing(), 0, 0);
+                }
+            } else {
+                Toast.makeText(this, "pickea una imagen",
+                        Toast.LENGTH_LONG).show();
+            }
+        } catch (Exception e) {
+            Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG)
+                    .show();
         }
-
-
-
-
-
-
-
-
-
-
     }
-
     private void loadImageCamera() {
         Bitmap img = BitmapFactory.decodeFile(path);
         if(img != null) {
@@ -380,5 +534,8 @@ public class PostProduct extends AppCompatActivity implements View.OnClickListen
 
         }
     }
+
+
+
 
 }
